722ebd6b462bb90f00c3156fa9f14d3c2e344902
Akash Saravanan <akashsara@outlook.com>
2022-07-13 00:25:45 -0600

    tested & working

--- test_algo_ppo.py
+++ test_algo_ppo.py
@@ -15,11 +15,11 @@ import torch.nn as nn
 import utils
 from models import simple_models
 from rl.agents.ppo_gym_compatible import PPOAgent
-from rl.memory import PPOMemory
 
 if __name__ == "__main__":
     # Config - Versioning
-    experiment_name = f"TestLanderPPO"
+    # experiment_name = f"TestLanderPPO"
+    experiment_name = f"TestCartpolePPO"
     hash_name = str(hash(experiment_name))[2:12]
 
     # Config - Model Save Directory
@@ -27,33 +27,33 @@ if __name__ == "__main__":
 
     # Config - Model Hyperparameters
     training_config = {
-        "batch_size": 64,
-        "log_interval": 1000,
+        "batch_size": 32,
         "num_training_epochs": 10,
         "gamma": 0.99,  # Discount Factor
-        "lambda_": 0.95,  # GAE Parameter
+        "gae_lambda": 0.95,  # GAE Parameter
         "clip_param": 0.2,  # Surrogate Clipping Parameter
+        "value_clip_param": 0.2,  # Value Function Clipping Parameter
         "c1": 0.5,  # Loss constant 1
         "c2": 0.002,  # Loss constant 2
         "normalize_advantages": False,
+        "use_action_mask": False,
     }
 
     # Config - Training Hyperparameters
     RANDOM_SEED = 42
-    NB_TRAINING_STEPS = 1000000
-    STEPS_PER_ROLLOUT = 2500  # Steps to gather before running PPO (train interval)
-    VALIDATE_EVERY = 200000  # Run intermediate evaluation every N steps
+    STEPS_PER_ROLLOUT = 2048  # Steps gathered before training (train interval)
+    VALIDATE_EVERY = 204800  # Run intermediate evaluation every N steps
+    NB_TRAINING_STEPS = 1024000  # Total training steps
     NB_VALIDATION_EPISODES = 100  # Intermediate Evaluation
     NB_EVALUATION_EPISODES = 1000  # Final Evaluation
 
     # Config = Model Setup
     MODEL = simple_models.SimpleActorCriticModel
     MODEL_KWARGS = {}
-    memory_config = {"batch_size": training_config["batch_size"]}
 
     # Config - Optimizer Setup
     OPTIMIZER = torch.optim.Adam
-    OPTIMIZER_KWARGS = {"lr": 3e-4}
+    OPTIMIZER_KWARGS = {"lr": 3e-4, "eps": 1e-5}
 
     # Set random seed
     np.random.seed(RANDOM_SEED)
@@ -65,22 +65,24 @@ if __name__ == "__main__":
         os.makedirs(output_dir)
 
     # Setup player
-    env = gym.make("LunarLander-v2")
+    # env = gym.make("LunarLander-v2")
+    env = gym.make("CartPole-v1")
 
     # Grab some values from the environment to setup our model
     print(env.action_space, env.observation_space)
     MODEL_KWARGS["n_actions"] = env.action_space.n
-    MODEL_KWARGS["n_obs"] = 8
+    # MODEL_KWARGS["n_obs"] = 8
+    MODEL_KWARGS["n_obs"] = 4
 
-    # Setup memory
-    memory = PPOMemory(**memory_config)
     # Defining our DQN
     ppo = PPOAgent(
         model=MODEL,
         model_kwargs=MODEL_KWARGS,
         optimizer=OPTIMIZER,
         optimizer_kwargs=OPTIMIZER_KWARGS,
-        memory=memory,
+        steps_per_rollout=STEPS_PER_ROLLOUT,
+        state_size=MODEL_KWARGS["n_obs"],
+        n_actions=MODEL_KWARGS["n_actions"],
         **training_config,
     )
 
@@ -94,11 +96,10 @@ if __name__ == "__main__":
         f"initial",
     )
 
-    last_validated = 0
-    num_epochs = max(VALIDATE_EVERY // STEPS_PER_ROLLOUT, 1)
-    while ppo.iterations < NB_TRAINING_STEPS:
+    num_epochs = max(NB_TRAINING_STEPS // VALIDATE_EVERY, 1)
+    for i in range(num_epochs):
         # Train Model
-        ppo.fit(env, steps_per_rollout=STEPS_PER_ROLLOUT, num_epochs=num_epochs)
+        ppo.fit(env, total_steps=VALIDATE_EVERY)
 
         # Save Model
         ppo.save(output_dir, reset_trackers=True, create_plots=False)
@@ -106,16 +107,13 @@ if __name__ == "__main__":
         # Evaluate Model
         # Works only if NB_VALIDATION_EPISODES is set
         # And this isn't the last "epoch" [Since we do a full eval after this]
-        if (
-            NB_VALIDATION_EPISODES > 0
-            and (ppo.iterations - last_validated) >= VALIDATE_EVERY
-        ):
+        if NB_VALIDATION_EPISODES > 0 and i + 1 != num_epochs:
             evaluation_results = utils.gym_env_validate_model(
                 evaluation_results,
                 env,
                 ppo,
                 NB_VALIDATION_EPISODES,
-                f"validation_{ppo.iterations}",
+                f"validation_{i+1}",
             )
 
     # Evaluation
