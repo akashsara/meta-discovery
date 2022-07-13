b80616ade238443df84955cc346f80856f473226
Akash Saravanan <akashsara@outlook.com>
2022-07-13 00:26:09 -0600

    add poke_env validation; update plots

--- utils.py
+++ utils.py
@@ -2,12 +2,64 @@ import os
 import torch
 
 
+def poke_env_validate_model(
+    env_player,
+    evaluation_func,
+    model,
+    num_episodes,
+    random_player,
+    max_player,
+    smax_player,
+    key,
+    results,
+):
+    print("Results against random player:")
+    env_player.play_against(
+        env_algorithm=evaluation_func,
+        opponent=random_player,
+        env_algorithm_kwargs={
+            "model": model,
+            "num_episodes": num_episodes,
+        },
+    )
+    random_wins = env_player.n_won_battles
+
+    print("\nResults against max player:")
+    env_player.play_against(
+        env_algorithm=evaluation_func,
+        opponent=max_player,
+        env_algorithm_kwargs={
+            "model": model,
+            "num_episodes": num_episodes,
+        },
+    )
+    max_wins = env_player.n_won_battles
+
+    print("\nResults against smart max player:")
+    env_player.play_against(
+        env_algorithm=evaluation_func,
+        opponent=smax_player,
+        env_algorithm_kwargs={
+            "model": model,
+            "num_episodes": num_episodes,
+        },
+    )
+    smax_wins = env_player.n_won_battles
+    results[key] = {
+        "num_episodes": num_episodes,
+        "vs_random": random_wins,
+        "vs_max": max_wins,
+        "vs_smart_max": smax_wins,
+    }
+    return results
+
+
 def gym_env_validate_model(results, env, model, episodes, key):
     average_rewards, average_episode_rewards = model.test(env, episodes)
     results[f"{key}"] = {
         "n_episodes": episodes,
         "average_rewards": average_rewards,
-        "average_episode_rewards": average_episode_rewards
+        "average_episode_rewards": average_episode_rewards,
     }
     print(f"{key} rewards: {average_rewards}, {average_episode_rewards}")
     return results
@@ -17,6 +69,7 @@ def load_trackers_to_ppo_model(output_dir, ppo):
     # Load back all the trackers to draw the final plots
     all_rewards = []
     all_episode_lengths = []
+    all_episode_returns = []
     all_actor_losses = []
     all_critic_losses = []
     all_entropy = []
@@ -36,6 +89,7 @@ def load_trackers_to_ppo_model(output_dir, ppo):
         )
         all_rewards.append(x["reward"])
         all_episode_lengths.append(x["episode_lengths"])
+        all_episode_returns.append(x["episode_returns"])
         all_actor_losses.append(x["actor_loss"])
         all_critic_losses.append(x["critic_loss"])
         all_entropy.append(x["entropy"])
@@ -43,6 +97,7 @@ def load_trackers_to_ppo_model(output_dir, ppo):
         all_approx_kl_divs.append(x["approx_kl_divs"])
     all_rewards = torch.cat(all_rewards).flatten().cpu().numpy()
     all_episode_lengths = torch.cat(all_episode_lengths).flatten().cpu().numpy()
+    all_episode_returns = torch.cat(all_episode_returns).flatten().cpu().numpy()
     all_actor_losses = torch.cat(all_actor_losses).flatten().cpu().numpy()
     all_critic_losses = torch.cat(all_critic_losses).flatten().cpu().numpy()
     all_entropy = torch.cat(all_entropy).flatten().cpu().numpy()
@@ -50,6 +105,7 @@ def load_trackers_to_ppo_model(output_dir, ppo):
     all_approx_kl_divs = torch.cat(all_approx_kl_divs).flatten().cpu().numpy()
     ppo.rewards = all_rewards
     ppo.episode_lengths = all_episode_lengths
+    ppo.episode_returns = all_episode_returns
     ppo.actor_losses = all_actor_losses
     ppo.critic_losses = all_critic_losses
     ppo.entropy = all_entropy
