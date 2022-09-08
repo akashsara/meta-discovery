8968555581ea409cac30963c8d8e47eb19ab1e0b
Akash Saravanan <akashsara@outlook.com>
2022-09-08 17:44:09 -0600

    update legality checker + ban lists

--- scripts/meta_discovery_utils.py
+++ scripts/meta_discovery_utils.py
@@ -42,6 +42,7 @@ def legality_checker(moveset_database, tier, ban_list):
     to_delete = []
     for pokemon, movesets in moveset_database.items():
         for moveset_name, moveset in movesets.items():
+            # Baton Pass is illegal in all tiers
             if (
                 "Baton Pass" in moveset
                 or "baton pass" in moveset
@@ -49,6 +50,40 @@ def legality_checker(moveset_database, tier, ban_list):
             ):
                 print(f"BATON PASS CLAUSE: {pokemon} - {moveset_name}")
                 to_delete.append((pokemon, moveset_name))
+            # Double Team & Minimize = Evasion Clause = Banned in all tiers
+            elif (
+                "Double Team" in moveset
+                or "double team" in moveset
+                or "doubleteam" in moveset
+            ):
+                print(f"EVASION CLAUSE: {pokemon} - {moveset_name}")
+                to_delete.append((pokemon, moveset_name))
+            elif "Minimize" in moveset or "minimize" in moveset:
+                print(f"EVASION CLAUSE: {pokemon} - {moveset_name}")
+                to_delete.append((pokemon, moveset_name))
+            # Fissure, Guillotine, Horn Drill, Sheer COld = OHKO Clause
+            # Banned in all tiers
+            elif "Fissure" in moveset or "fissure" in moveset:
+                print(f"OHKO CLAUSE: {pokemon} - {moveset_name}")
+                to_delete.append((pokemon, moveset_name))
+            elif "Guillotine" in moveset or "guillotine" in moveset:
+                print(f"OHKO CLAUSE: {pokemon} - {moveset_name}")
+                to_delete.append((pokemon, moveset_name))
+            elif (
+                "Horn Drill" in moveset
+                or "horn drill" in moveset
+                or "horndrill" in moveset
+            ):
+                print(f"OHKO CLAUSE: {pokemon} - {moveset_name}")
+                to_delete.append((pokemon, moveset_name))
+            elif "Sheer Cold" in moveset or "sheer cold" in moveset:
+                print(f"OHKO CLAUSE: {pokemon} - {moveset_name}")
+                to_delete.append((pokemon, moveset_name))
+            # Moody Clause - banned below Ubers
+            elif tier != "ubers" and ("Moody" in moveset or "moody" in moveset):
+                print(f"MOODY CLAUSE: {pokemon} - {moveset_name}")
+                to_delete.append((pokemon, moveset_name))
+            # Arena Trap is banned below Ubers
             elif tier != "ubers" and (
                 "Arena Trap" in moveset
                 or "arena trap" in moveset
@@ -56,6 +91,7 @@ def legality_checker(moveset_database, tier, ban_list):
             ):
                 print(f"ARENA TRAP CLAUSE: {pokemon} - {moveset_name}")
                 to_delete.append((pokemon, moveset_name))
+            # Shadow Tag is banned below Ubers
             elif tier != "ubers" and (
                 "Shadow Tag" in moveset
                 or "shadow tag" in moveset
@@ -63,6 +99,7 @@ def legality_checker(moveset_database, tier, ban_list):
             ):
                 print(f"SHADOW TAG CLAUSE: {pokemon} - {moveset_name}")
                 to_delete.append((pokemon, moveset_name))
+            # Power Construct is banned below Ubers
             elif tier != "ubers" and (
                 "Power Construct" in moveset
                 or "power construct" in moveset
@@ -70,6 +107,7 @@ def legality_checker(moveset_database, tier, ban_list):
             ):
                 print(f"POWER CONSTRUCT CLAUSE: {pokemon} - {moveset_name}")
                 to_delete.append((pokemon, moveset_name))
+            # Light Clay is banned below OU
             elif tier not in ["ubers", "ou"] and (
                 "Light Clay" in moveset
                 or "light clay" in moveset
@@ -77,6 +115,7 @@ def legality_checker(moveset_database, tier, ban_list):
             ):
                 print(f"LIGHT CLAY CLAUSE: {pokemon} - {moveset_name}")
                 to_delete.append((pokemon, moveset_name))
+            # King's Rock is banned below OU
             elif tier not in ["ubers", "ou"] and (
                 "King's Rock" in moveset
                 or "Kings Rock" in moveset
@@ -87,6 +126,10 @@ def legality_checker(moveset_database, tier, ban_list):
             ):
                 print(f"KING'S ROCK CLAUSE: {pokemon} - {moveset_name}")
                 to_delete.append((pokemon, moveset_name))
+
+    print("Deleting Illegal Movesets")
+    for (pokemon, moveset) in to_delete:
+        del moveset_database[pokemon][moveset]
         # If we run into a scenario where there are no movesets for a Pokemon
         # We simply add it to the ban list
         # Since a ban has caused us to remove this moveset
@@ -94,7 +137,7 @@ def legality_checker(moveset_database, tier, ban_list):
         if len(moveset_database[pokemon]) == 0:
             ban_list.append(pokemon)
             print(f"NO MOVESETS LEFT: {pokemon}, {moveset_database[pokemon]}")
-        
+
     return moveset_database, ban_list
 
 
@@ -140,5 +183,60 @@ all_tiers_banlist = {
         "zamazentacrowned",
         "zekrom",
         "zygarde",
+    ],
+    "uubl": [
+        "alakazam",
+        "arctozolt",
+        "blaziken",
+        "dracozolt",
+        "gengar",
+        "hawlucha",
+        "kommoo",
+        "latias",
+        "latios",
+        "mienshao",
+        "moltresgalar",
+        "terrakion",
+        "thundurus",
+    ],
+    "uu": [
+        "barraskewda",
+        "bisharp",
+        "blacephalon",
+        "blissey",
+        "buzzwole",
+        "clefable",
+        "corviknight",
+        "dragapult",
+        "dragonite",
+        "ferrothorn",
+        "garchomp",
+        "heatran",
+        "kartana",
+        "landorustherian",
+        "magnezone",
+        "mandibuzz",
+        "melmetal",
+        "mew",
+        "ninetalesalola",
+        "pelipper",
+        "regieleki",
+        "rillaboom",
+        "slowbro",
+        "slowkinggalar",
+        "tapufini",
+        "tapukoko",
+        "tapulele",
+        "tornadustherian",
+        "toxapex",
+        "tyranitar",
+        "urshifurapidstrike",
+        "victini",
+        "volcanion",
+        "volcarona",
+        "weavile",
+        "zapdos",
+        "zapdosgalar",
+        "zeraora",
     ],
 }
