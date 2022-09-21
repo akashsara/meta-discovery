41f96aa932aa1b770120fdcec13e9755eba00450
Akash Saravanan <akashsara@outlook.com>
2022-09-21 11:49:24 -0600

    fix: add pickrate plotting script

--- src/graphics.py
+++ src/graphics.py
@@ -9,3 +9,14 @@ def plot_and_save_loss(values, xlabel, ylabel, output_path):
     ax.set_ylabel(ylabel)
     plt.tight_layout()
     plt.savefig(output_path)
+    plt.close()
+
+
+def plot_pickrates(data, title, output_path):
+    plt.figure(dpi=300)
+    x = list(range(len(data)))
+    plt.bar(x, data)
+    plt.title(title)
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
