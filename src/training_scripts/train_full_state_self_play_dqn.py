eaacafee4b32013b7fcea1d5a26d315b080a7540
Akash Saravanan <akashsara@outlook.com>
2022-09-28 18:34:51 -0600

    Fix: Upgrade full state selfplay models to new API

--- src/training_scripts/train_full_state_self_play_dqn.py
+++ src/training_scripts/train_full_state_self_play_dqn.py
@@ -5,6 +5,7 @@ import asyncio
 import json
 import os
 import sys
+import time
 from threading import Thread
 
 sys.path.append("./")
@@ -12,84 +13,75 @@ sys.path.append("./")
 import numpy as np
 import torch
 import torch.nn as nn
+import training_utils as utils
 from agents.full_state_agent import FullStatePlayer
 from agents.max_damage_agent import MaxDamagePlayer
 from agents.smart_max_damage_agent import SmartMaxDamagePlayer
 from models import full_state_models
 from poke_env.player.random_player import RandomPlayer
 from poke_env.player_configuration import PlayerConfiguration
-from poke_env.utils import to_id_str
 from rl.agents.dqn import DQNAgent
 from rl.memory import SequentialMemory
-from rl.policy import (ExponentialDecayEpsilonGreedyPolicy,
-                       LinearDecayEpsilonGreedyPolicy)
+from rl.policy import (
+    ExponentialDecayEpsilonGreedyPolicy,
+    LinearDecayEpsilonGreedyPolicy,
+)
 
 
-# This is the function that will be used to train the dqn
-def model_training(player, model, nb_steps):
-    model.fit(player, num_training_steps=nb_steps)
-    player.complete_current_battle()
-
-
-def model_evaluation(player, model, nb_episodes):
-    # Reset battle statistics
-    player.reset_battles()
-    model.test(player, num_episodes=nb_episodes)
-
-    print(
-        "Evaluation: %d victories out of %d episodes"
-        % (player.n_won_battles, nb_episodes)
+async def battle_handler(player1, player2, num_challenges):
+    await asyncio.gather(
+        player1.agent.accept_challenges(player2.username, num_challenges),
+        player2.agent.send_challenges(player1.username, num_challenges),
     )
 
 
-async def launch_battles(player, opponent):
-    battles_coroutine = asyncio.gather(
-        player.send_challenges(
-            opponent=to_id_str(opponent.username),
-            n_challenges=1,
-            to_wait=opponent.logged_in,
-        ),
-        opponent.accept_challenges(opponent=to_id_str(player.username), n_challenges=1),
-    )
-    await battles_coroutine
+def training_function(player, opponent, model, model_kwargs):
+    # Fit (train) model as necessary.
+    model.fit(player, **model_kwargs)
+    player.done_training = True
+    # Play out the remaining battles so both fit() functions complete
+    # We use 99 to give the agent an invalid option so it's forced
+    # to take a random legal action
+    while not opponent.done_training:
+        _, _, done, _ = player.step(99)
+        if done and not opponent.done_training:
+            _ = player.reset()
+            done = False
 
-
-def env_algorithm_wrapper(env_algorithm, player, kwargs):
-    env_algorithm(player, **kwargs)
-
-    player._start_new_battle = False
-    while True:
-        try:
-            player.complete_current_battle()
-            player.reset()
-        except OSError:
-            break
+    # Forfeit any ongoing battles
+    while player.current_battle and not player.current_battle.finished:
+        _ = player.step(-1)
 
 
 if __name__ == "__main__":
     # Config - Versioning
-    experiment_name = f"New_FullState_DQN_SelfPlay_v2"
+    training_opponent = "random"  # random, max, smart
+    experiment_name = f"New_FullState_DQN_SelfPlay_v1"
+    server_port = 8000
     hash_name = str(hash(experiment_name))[2:12]
+    expt_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    print(f"Experiment: {experiment_name}\t Time: {expt_time}")
+    start_time = time.time()
 
     # Config - Model Save Directory
     model_dir = "models"
 
+    # Config - Training Hyperparameters
+    RANDOM_SEED = 42
+    NB_TRAINING_STEPS = 100000  # Total training steps
+    VALIDATE_EVERY = 50000  # Run intermediate evaluation every N steps
+    NB_VALIDATION_EPISODES = 100  # Intermediate Evaluation
+    NB_EVALUATION_EPISODES = 1000  # Final Evaluation
+
     # Config - Model Hyperparameters
     training_config = {
         "batch_size": 32,
-        "gamma": 0.9,
-        "tau": 1000,  # AKA Target Model Update
+        "gamma": 0.95,  # Discount Factor
+        "tau": 1,  # AKA Target Model Update
         "train_interval": 1,
         "log_interval": 1000,
         "warmup_steps": 1000,
     }
-
-    # Config - Training Hyperparameters
-    RANDOM_SEED = 42
-    NB_TRAINING_STEPS = 10000
-    VALIDATE_EVERY = 5000  # Run intermediate evaluation every N steps
-    NB_VALIDATION_EPISODES = 100  # Intermediate Evaluation
-    NB_EVALUATION_EPISODES = 1000  # Final Evaluation
 
     # Config = Model Setup
     MODEL = full_state_models.BattleModel
@@ -97,12 +89,15 @@ if __name__ == "__main__":
         "pokemon_embedding_dim": 32,
         "team_embedding_dim": 64,
     }
-    memory_config = {"capacity": 10000}
+
+    # Config - Memory Setup
+    MEMORY = SequentialMemory
+    MEMORY_KWARGS = {"capacity": 10000}
 
     # Config - Policy Setup
     POLICY = LinearDecayEpsilonGreedyPolicy
     # POLICY = ExponentialDecayEpsilonGreedyPolicy
-    policy_config = {
+    POLICY_KWARGS = {
         "max_epsilon": 0.95,
         "min_epsilon": 0.05,
         # "epsilon_decay": 1000,
@@ -112,7 +107,7 @@ if __name__ == "__main__":
     # Config - Optimizer Setup
     OPTIMIZER = torch.optim.Adam
     OPTIMIZER_KWARGS = {"lr": 0.00025}
-    
+
     # Config - Loss Setup
     LOSS = nn.SmoothL1Loss
     LOSS_KWARGS = {
@@ -132,10 +127,15 @@ if __name__ == "__main__":
     np.random.seed(RANDOM_SEED)
     _ = torch.manual_seed(RANDOM_SEED)
 
+    # Setup server configuration
+    # Maintain servers on different ports to avoid Compute Canada errors
+    server_config = utils.generate_server_configuration(server_port)
+
     # Setup agent usernames for connecting to local showdown
     # This lets us train multiple agents while connecting to the same server
     training_agent1 = PlayerConfiguration(hash_name + "_P1", None)
     training_agent2 = PlayerConfiguration(hash_name + "_P2", None)
+    test_agent = PlayerConfiguration(hash_name + "_Test", None)
     rand_player = PlayerConfiguration(hash_name + "_Rand", None)
     max_player = PlayerConfiguration(hash_name + "_Max", None)
     smax_player = PlayerConfiguration(hash_name + "_SMax", None)
@@ -146,12 +146,36 @@ if __name__ == "__main__":
         os.makedirs(output_dir)
     config["lookup_filename"] = os.path.join(output_dir, config["lookup_filename"])
 
+    # Setup opponents
+    random_agent = RandomPlayer(
+        battle_format="gen8randombattle",
+        player_configuration=rand_player,
+        server_configuration=server_config,
+        start_timer_on_battle_start=True
+    )
+    max_damage_agent = MaxDamagePlayer(
+        battle_format="gen8randombattle",
+        player_configuration=max_player,
+        server_configuration=server_config,
+        start_timer_on_battle_start=True
+    )
+    smart_max_damage_agent = SmartMaxDamagePlayer(
+        battle_format="gen8randombattle",
+        player_configuration=smax_player,
+        server_configuration=server_config,
+        start_timer_on_battle_start=True
+    )
+
     # Setup player
     player1 = FullStatePlayer(
         config,
         battle_format="gen8randombattle",
         log_level=30,
         player_configuration=training_agent1,
+        server_configuration=server_config,
+        opponent=None,
+        start_challenging=False,
+        start_timer_on_battle_start=True
     )
     config["create"] = False
     player2 = FullStatePlayer(
@@ -159,172 +183,140 @@ if __name__ == "__main__":
         battle_format="gen8randombattle",
         log_level=30,
         player_configuration=training_agent2,
+        server_configuration=server_config,
+        opponent=None,
+        start_challenging=False,
+        start_timer_on_battle_start=True
     )
-
-    # Setup opponents
-    random_agent = RandomPlayer(
-        battle_format="gen8randombattle", player_configuration=rand_player
-    )
-    max_damage_agent = MaxDamagePlayer(
-        battle_format="gen8randombattle", player_configuration=max_player
-    )
-    smart_max_damage_agent = SmartMaxDamagePlayer(
-        battle_format="gen8randombattle", player_configuration=smax_player
+    # Setup independent player for testing
+    test_player = FullStatePlayer(
+        config,
+        battle_format="gen8randombattle",
+        log_level=30,
+        player_configuration=test_agent,
+        server_configuration=server_config,
+        opponent=None,
+        start_challenging=False,
+        start_timer_on_battle_start=True
     )
 
     # Grab some values from the environment to setup our model
-    MODEL_KWARGS["n_actions"] = len(player1.action_space)
+    state = player1.create_empty_state_vector()
+    state = player1.state_to_machine_readable_state(state)
+    state_size = state.shape[0]
+    n_actions = player1.action_space.n
+    MODEL_KWARGS["n_actions"] = n_actions
     MODEL_KWARGS["state_length_dict"] = player1.get_state_lengths()
     MODEL_KWARGS["max_values_dict"] = player1.lookup["max_values"]
 
     # Setup memory
-    memory = SequentialMemory(**memory_config)
+    memory = MEMORY(**MEMORY_KWARGS)
 
     # Simple Epsilon Greedy Policy
-    policy = POLICY(**policy_config)
+    policy = POLICY(**POLICY_KWARGS)
+
+    # Setup loss function
+    loss = LOSS(**LOSS_KWARGS)
 
     # Defining our DQN
     dqn = DQNAgent(
         policy=policy,
         memory=memory,
+        loss_function=loss,
         model=MODEL,
         model_kwargs=MODEL_KWARGS,
         optimizer=OPTIMIZER,
         optimizer_kwargs=OPTIMIZER_KWARGS,
-        loss=LOSS,
-        loss_kwargs=LOSS_KWARGS,
         **training_config,
     )
 
     evaluation_results = {}
-    epochs = max(NB_TRAINING_STEPS // VALIDATE_EVERY, 1)
-    for i in range(epochs):
+    if NB_VALIDATION_EPISODES > 0:
+        evaluation_results = utils.poke_env_validate_model(
+            test_player,
+            dqn,
+            NB_VALIDATION_EPISODES,
+            random_agent,
+            max_damage_agent,
+            smart_max_damage_agent,
+            f"initial",
+            evaluation_results,
+        )
+    num_epochs = max(NB_TRAINING_STEPS // VALIDATE_EVERY, 1)
+
+    for i in range(num_epochs):
         # Setup arguments to pass to the training function
-        p1_env_kwargs = {"model": dqn, "nb_steps": VALIDATE_EVERY}
-        p2_env_kwargs = {"model": dqn, "nb_steps": VALIDATE_EVERY}
+        p1_env_kwargs = {
+            "num_training_steps": VALIDATE_EVERY,
+            "do_training": True,
+        }
+        p2_env_kwargs = {
+            "num_training_steps": VALIDATE_EVERY,
+            "do_training": False,
+        }
 
-        # Train Model
-        # Make Two Threads And Play vs Each Other
-        player1._start_new_battle = True
-        player2._start_new_battle = True
-
+        # Self-Play bits
+        player1.done_training = False
+        player2.done_training = False
+        # 1. Get event loop
         loop = asyncio.get_event_loop()
-
+        # Make Two Threads; one per player and run model.fit()
         t1 = Thread(
-            target=lambda: env_algorithm_wrapper(model_training, player1, p1_env_kwargs)
+            target=lambda: training_function(player1, player2, dqn, p1_env_kwargs)
         )
         t1.start()
 
         t2 = Thread(
-            target=lambda: env_algorithm_wrapper(model_training, player2, p2_env_kwargs)
+            target=lambda: training_function(player2, player1, dqn, p2_env_kwargs)
         )
         t2.start()
-
-        while player1._start_new_battle:
-            loop.run_until_complete(launch_battles(player1, player2))
+        # On the network side, keep sending & accepting battles
+        while not player1.done_training or not player2.done_training:
+            loop.run_until_complete(battle_handler(player1, player2, 1))
+        # Wait for thread completion
         t1.join()
         t2.join()
 
-        # Save model
-        dqn.save(output_dir, reset_trackers=True, create_plots=False)
+        player1.close(purge=False)
+        player2.close(purge=False)
 
         # Evaluate Model
-        # Works only if NB_VALIDATION_EPISODES is set
-        # And this isn't the last "epoch" [Since we do a full eval after this]
-        if NB_VALIDATION_EPISODES > 0 and i + 1 != epochs:
-            evaluation_results[f"validation_set_{i+1}"] = {
-                "n_battles": NB_VALIDATION_EPISODES,
-            }
-
-            print("Results against random player:")
-            player1.play_against(
-                env_algorithm=model_evaluation,
-                opponent=random_agent,
-                env_algorithm_kwargs={
-                    "model": dqn,
-                    "nb_episodes": NB_VALIDATION_EPISODES,
-                },
+        if NB_VALIDATION_EPISODES > 0 and i + 1 != num_epochs:
+            # Save model
+            dqn.save(output_dir, reset_trackers=True, create_plots=False)
+            # Validation
+            evaluation_results = utils.poke_env_validate_model(
+                test_player,
+                dqn,
+                NB_VALIDATION_EPISODES,
+                random_agent,
+                max_damage_agent,
+                smart_max_damage_agent,
+                f"validation_{i+1}",
+                evaluation_results,
             )
-            evaluation_results[f"validation_set_{i+1}"][
-                "vs_random"
-            ] = player1.n_won_battles
-
-            print("\nResults against max player:")
-            player1.play_against(
-                env_algorithm=model_evaluation,
-                opponent=max_damage_agent,
-                env_algorithm_kwargs={
-                    "model": dqn,
-                    "nb_episodes": NB_VALIDATION_EPISODES,
-                },
-            )
-            evaluation_results[f"validation_set_{i+1}"][
-                "vs_max"
-            ] = player1.n_won_battles
-
-            print("\nResults against smart max player:")
-            player1.play_against(
-                env_algorithm=model_evaluation,
-                opponent=smart_max_damage_agent,
-                env_algorithm_kwargs={
-                    "model": dqn,
-                    "nb_episodes": NB_VALIDATION_EPISODES,
-                },
-            )
-            evaluation_results[f"validation_set_{i+1}"][
-                "vs_smax"
-            ] = player1.n_won_battles
+    # Save final model
+    dqn.save(output_dir, reset_trackers=True, create_plots=False)
 
     # Evaluation
     if NB_EVALUATION_EPISODES > 0:
-        evaluation_results["final"] = {
-            "n_battles": NB_EVALUATION_EPISODES,
-        }
-
-        print("Results against random player:")
-        player1.play_against(
-            env_algorithm=model_evaluation,
-            opponent=random_agent,
-            env_algorithm_kwargs={"model": dqn, "nb_episodes": NB_EVALUATION_EPISODES},
+        evaluation_results = utils.poke_env_validate_model(
+            test_player,
+            dqn,
+            NB_EVALUATION_EPISODES,
+            random_agent,
+            max_damage_agent,
+            smart_max_damage_agent,
+            f"final",
+            evaluation_results,
         )
-        evaluation_results["final"]["vs_random"] = player1.n_won_battles
-
-        print("\nResults against max player:")
-        player1.play_against(
-            env_algorithm=model_evaluation,
-            opponent=max_damage_agent,
-            env_algorithm_kwargs={"model": dqn, "nb_episodes": NB_EVALUATION_EPISODES},
-        )
-        evaluation_results["final"]["vs_max"] = player1.n_won_battles
-
-        print("\nResults against smart max player:")
-        player1.play_against(
-            env_algorithm=model_evaluation,
-            opponent=smart_max_damage_agent,
-            env_algorithm_kwargs={"model": dqn, "nb_episodes": NB_EVALUATION_EPISODES},
-        )
-        evaluation_results["final"]["vs_smax"] = player1.n_won_battles
 
     with open(os.path.join(output_dir, "results.json"), "w") as fp:
         json.dump(evaluation_results, fp)
 
-    # Load back all the trackers to draw the final plots
-    all_losses = []
-    all_rewards = []
-    all_episode_lengths = []
-    # Sort files by iteration for proper graphing
-    files_to_read = sorted([int(file.split(".pt")[0].split("_")[1]) for file in os.listdir(output_dir) if "statistics_" in file])
-    for file in files_to_read:
-        x = torch.load(os.path.join(output_dir, f"statistics_{file}.pt"), map_location=dqn.device)
-        all_losses.append(x["loss"])
-        all_rewards.append(x["reward"])
-        all_episode_lengths.append(x["episode_lengths"])
-    all_losses = torch.cat(all_losses).flatten().cpu().numpy()
-    all_rewards = torch.cat(all_rewards).flatten().cpu().numpy()
-    all_episode_lengths = torch.cat(all_episode_lengths).flatten().cpu().numpy()
-    dqn.losses = all_losses
-    dqn.rewards = all_rewards
-    dqn.episode_lengths = all_episode_lengths
+    utils.load_trackers_to_dqn_model(output_dir, dqn)
     dqn.plot_and_save_metrics(
         output_dir, is_cumulative=True, reset_trackers=True, create_plots=True
     )
+    end_time = time.time()
+    print(f"Running Time: {end_time - start_time}")
--- src/training_scripts/train_full_state_self_play_ppo.py
+++ src/training_scripts/train_full_state_self_play_ppo.py
@@ -29,15 +29,22 @@ async def battle_handler(player1, player2, num_challenges):
     )
 
 
-def training_function(player, model, model_kwargs):
+def training_function(player, opponent, model, model_kwargs):
     # Fit (train) model as necessary.
     model.fit(player, **model_kwargs)
     player.done_training = True
     # Play out the remaining battles so both fit() functions complete
     # We use 99 to give the agent an invalid option so it's forced
     # to take a random legal action
+    while not opponent.done_training:
+        _, _, done, _ = player.step(99)
+        if done and not opponent.done_training:
+            _ = player.reset()
+            done = False
+    
+    # Forfeit any ongoing battles
     while player.current_battle and not player.current_battle.finished:
-        _ = player.step(99)
+        _ = player.step(-1)
 
 
 if __name__ == "__main__":
@@ -122,16 +129,19 @@ if __name__ == "__main__":
         battle_format="gen8randombattle",
         player_configuration=rand_player,
         server_configuration=server_config,
+        start_timer_on_battle_start=True
     )
     max_damage_agent = MaxDamagePlayer(
         battle_format="gen8randombattle",
         player_configuration=max_player,
         server_configuration=server_config,
+        start_timer_on_battle_start=True
     )
     smart_max_damage_agent = SmartMaxDamagePlayer(
         battle_format="gen8randombattle",
         player_configuration=smax_player,
         server_configuration=server_config,
+        start_timer_on_battle_start=True
     )
 
     # Setup player
@@ -141,8 +151,9 @@ if __name__ == "__main__":
         log_level=30,
         player_configuration=training_agent1,
         server_configuration=server_config,
-        opponent="placeholder",
+        opponent=None,
         start_challenging=False,
+        start_timer_on_battle_start=True
     )
     config["create"] = False
     player2 = FullStatePlayer(
@@ -151,8 +162,9 @@ if __name__ == "__main__":
         log_level=30,
         player_configuration=training_agent2,
         server_configuration=server_config,
-        opponent="placeholder",
+        opponent=None,
         start_challenging=False,
+        start_timer_on_battle_start=True
     )
     # Setup independent player for testing
     test_player = FullStatePlayer(
@@ -161,8 +173,9 @@ if __name__ == "__main__":
         log_level=30,
         player_configuration=test_agent,
         server_configuration=server_config,
-        opponent="placeholder",
+        opponent=None,
         start_challenging=False,
+        start_timer_on_battle_start=True
     )
 
     # Grab some values from the environment to setup our model
@@ -198,8 +211,8 @@ if __name__ == "__main__":
             f"initial",
             evaluation_results,
         )
-
     num_epochs = max(NB_TRAINING_STEPS // VALIDATE_EVERY, 1)
+
     for i in range(num_epochs):
         # Setup arguments to pass to the training function
         p1_env_kwargs = {
@@ -217,10 +230,10 @@ if __name__ == "__main__":
         # 1. Get event loop
         loop = asyncio.get_event_loop()
         # Make Two Threads; one per player and run model.fit()
-        t1 = Thread(target=lambda: training_function(player1, ppo, p1_env_kwargs))
+        t1 = Thread(target=lambda: training_function(player1, player2, ppo, p1_env_kwargs))
         t1.start()
 
-        t2 = Thread(target=lambda: training_function(player2, ppo, p2_env_kwargs))
+        t2 = Thread(target=lambda: training_function(player2, player1, ppo, p2_env_kwargs))
         t2.start()
         # On the network side, keep sending & accepting battles
         while not player1.done_training or not player2.done_training:
@@ -247,6 +260,7 @@ if __name__ == "__main__":
                 f"validation_{i+1}",
                 evaluation_results,
             )
+
     # Save final model
     ppo.save(output_dir, reset_trackers=True, create_plots=False)
 
