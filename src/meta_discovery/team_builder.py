Fix: Incorrect proba calculation after masking

src/meta_discovery/team_builder.py
@@ -174,10 +174,9 @@ class TeamBuilder(Teambuilder):
         """
         np.choice requires probabilities not weights.
         """
-        summation = weights.sum()
-        if summation:
+        if weights.sum() > 0:
             weights[mask_ids] = 0
-            return weights / summation
+            return weights / weights.sum()
         else:
             probs = (weights + 1) / (weights.shape[0] - len(mask_ids))
             # Ensure that bans remain banned
@@ -260,7 +259,7 @@ class TeamBuilder(Teambuilder):
                         axis=0
                     ) / len(team_indices)
                     ## Compute overall weights
-                    term1 = self.c1[len(team)] * (bst_score + type_score)
+                    term1 = self.c1[len(team)] * (0.5 * bst_score + 0.2 * meta_type_score * 0.25 * type_score)
                     term2 = self.c2[len(team)] * (popularity_score)
                     weights = term1 + term2
 